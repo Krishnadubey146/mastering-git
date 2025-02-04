263de93c8b7e64740d0ca2dd71ddbc5c90cc3548
Krishna <Krishnaadubey123@gmail.com>
2025-02-04 21:33:09 +0530

    Add hello and test files

---
+++ hello.kt
@@ -0,0 +1,11 @@
+class hello {
+        // Correct: Members inside a clas
+        fun sayHello() {
+            println("Hi! Git")
+        }
+
+}
+fun main(){
+    val greeter = hello()
+    greeter.sayHello()
+}
---
+++ test.kt
@@ -0,0 +1,11 @@
+class test {
+    // Correct: Members inside a clas
+    fun sayHello() {
+        println("Hi! Git family!!")
+    }
+
+}
+fun main(){
+    val greeter = test()
+    greeter.sayHello()
+}
